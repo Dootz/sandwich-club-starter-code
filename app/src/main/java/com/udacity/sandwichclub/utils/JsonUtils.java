4e76a9a76efe9b030db4bd4443bdfe753850261f
Dootz <k.kozubek@wp.pl>
2018-02-15 23:22:45 +0100

    JSON parsing + layout

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,48 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class JsonUtils {
 
+    public static final String TAG = JsonUtils.class.getSimpleName();
+
     public static Sandwich parseSandwichJson(String json) {
+        try {
+            JSONObject sandwichJSON = new JSONObject(json);
+            JSONObject name = sandwichJSON.getJSONObject("name");
+            String mainName = name.getString("mainName");
+            JSONArray alsoKnownAsArray = name.getJSONArray("alsoKnownAs");
+            List<String> alsoKnownAs = new ArrayList<String>();
+            for (int i=0; i<alsoKnownAsArray.length(); i++) {
+                alsoKnownAs.add( alsoKnownAsArray.getString(i) );
+            }
+            String placeOfOrigin = sandwichJSON.getString("placeOfOrigin");
+
+            String description = sandwichJSON.getString("description");
+            String image = sandwichJSON.getString("image");
+            JSONArray ingredientsArray = sandwichJSON.getJSONArray("ingredients");
+
+            List<String> ingredients = new ArrayList<String>();
+            for (int i=0; i<ingredientsArray.length(); i++) {
+                ingredients.add( ingredientsArray.getString(i) );
+            }
+            Sandwich sandwich = new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+
+            return sandwich;
+        }
+        catch(JSONException ex){
+            Log.e(TAG, "Parsing error: " + ex.getMessage());
+        }
         return null;
     }
 }
